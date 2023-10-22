e7be4a1b0952888710579052a6bccae280d5b288
Pavithran <pavithran.soft@gmail.com>
2023-10-22 21:30:29 +0530

    deploy commit

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -6,7 +6,7 @@ const Card = (props) => {
 
   return (
     <div
-      className="md:h-36 lg:h-40 rounded-lg bg-white border grid md:grid-cols-[20%,80%] py-3 cursor-pointer text-xs md:text-sm lg:text-base px-2 hover:border-[#E54065] active:bg-[#F2F2F2] focus:bg-[#F2F2F2]"
+      className="md:h-32 lg:h-36 rounded-lg bg-white border grid md:grid-cols-[20%,80%] py-3 cursor-pointer text-xs md:text-sm lg:text-base px-2 hover:border-[#E54065] active:bg-[#F2F2F2] focus:bg-[#F2F2F2]"
       onClick={() => console.log(props.id)}
     >
       <span className="justify-self-center rounded-full bg-[#E54065] text-white font-semibold h-fit px-2 py-3">
